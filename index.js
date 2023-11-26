32235cdbd140ab3d870b8e71717d9f3fbcfd1bdf
maamokun <maamo-kun@outlook.com>
2023-11-26 22:08:43 +0900

    added simple error handling

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const app = express();
 
 app.get('/nahida', async (req, res) => {
     const query = req.query;
-    await generate(query).then((buffer) => {
+    try {
+        const buffer = await generate(query);
         res.set('Content-Type', 'image/png');
         res.send(buffer);
-    });
+    } catch (e) {
+        console.log(e);
+        res.status(500).send('Internal Server Error');
+    }
 });
 
 app.listen(3000, () => {
